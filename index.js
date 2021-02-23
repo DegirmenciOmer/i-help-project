1c0974007341d88553d8cb44e130110b73953b82
avamoradi <ava.moradi86@gmail.com>
2021-02-23 10:49:15 +0100

    delete node-module

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { ApolloServer } = require('apollo-server');
 const mongoose = require('mongoose');
 
-const { MONGODB } = require('./config.js');
+const { MONGODB } = require('./config');
 
 const typeDefs = require('./graphql/typeDefs');
 const resolvers = require('./graphql/resolvers');
