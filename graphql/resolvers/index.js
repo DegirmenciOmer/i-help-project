c5ac8850596d686c5a7f30a7b47f0969e2cb097b
avamoradi <ava.moradi86@gmail.com>
2021-02-23 09:59:05 +0100

    add files

---
+++ graphql/resolvers/index.js
@@ -0,0 +1,12 @@
+const postsResolvers = require('./posts');
+const userResolvers = require('./users');
+
+module.exports = {
+  Query: {
+    ...postsResolvers.Query,
+  },
+
+  Mutation: {
+    ...userResolvers.Mutation,
+  },
+};
---
+++ index.js
@@ -0,0 +1,30 @@
+const { ApolloServer } = require('apollo-server');
+const mongoose = require('mongoose');
+
+const { MONGODB } = require('./config.js');
+
+const typeDefs = require('./graphql/typeDefs');
+const resolvers = require('./graphql/resolvers');
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+mongoose
+  .connect(MONGODB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log('MongoDB Connected');
+    return server.listen({ port: 5000 });
+  })
+  .then((res) => {
+    console.log(`Server running at ${res.url}`);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
