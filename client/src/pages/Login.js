181e47644ac698f6f537bb976ee6e36d2a69bf91
serafima <svp1982@mail.ru>
2021-02-25 15:52:36 +0100

    add auth functionality

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,11 +1,14 @@
-import React, {useState} from 'react';
+import React, {useState, useContext} from 'react';
 import { Button, Form } from 'semantic-ui-react';
 import {useMutation} from '@apollo/client';
 import gql from 'graphql-tag';
 
+import {AuthContext} from '../context/auth';
+
 import {useForm} from '../util/hooks';
 
 const Login = (props) => {
+    const context = useContext(AuthContext);
     const [errors, setErrors] = useState({});
 
     const { onChange, onSubmit, values } = useForm(loginUserCallback, {
@@ -14,8 +17,8 @@ const Login = (props) => {
     } )
     
     const [loginUser, {loading}] = useMutation(LOGIN_USER, {
-        update(_, result){
-            console.log(result);
+        update(_, {data: {login: userData}}){
+            context.login(userData)
             props.history.push('/');
         },
         onError(err) {
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,11 +1,14 @@
-import React, {useState} from 'react';
+import React, {useState, useContext} from 'react';
 import { Button, Form } from 'semantic-ui-react';
 import {useMutation} from '@apollo/client';
 import gql from 'graphql-tag';
 
+import {AuthContext} from '../context/auth';
+
 import {useForm} from '../util/hooks';
 
 const Register = (props) => {
+    const context = useContext(AuthContext);
     const [errors, setErrors] = useState({});
 
     const { onChange, onSubmit, values } = useForm(registerUser, {
@@ -16,8 +19,8 @@ const Register = (props) => {
     } )
     
     const [addUser, {loading}] = useMutation(REGISTER_USER, {
-        update(_, result){
-            console.log(result);
+        update(_, {data: {register: userData}}){
+            context.login(userData);
             props.history.push('/');
         },
         onError(err) {
