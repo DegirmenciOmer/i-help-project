0d29060babd559dd4b30e0929e3c5d0a37425b4d
avamoradi <ava.moradi86@gmail.com>
2021-03-01 16:04:18 +0100

    final

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const typeDefs = require('./graphql/typeDefs');
 const resolvers = require('./graphql/resolvers');
 const pubsub = new PubSub();
 
-const PORT = process.env.port || 5000;
+const PORT = process.env.PORT || 5000;
 
 const server = new ApolloServer({
   typeDefs,
