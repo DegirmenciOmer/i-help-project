95ea3ec1f08542f16534ee0c59d71dfddc2760ee
serafima <svp1982@mail.ru>
2021-03-03 13:59:36 +0100

    fix mistakes

--- models/User.js
+++ models/User.js
@@ -1,7 +1,7 @@
 const { model, Schema } = require('mongoose');
 
 const userSchema = new Schema({
-<<<<<<< HEAD
+
     username: String,
     email: String,
     password: String,
@@ -9,12 +9,4 @@ const userSchema = new Schema({
 })
 
 module.exports = model('User', userSchema);
-=======
-  username: String,
-  password: String,
-  email: String,
-  createdAt: String,
-});
 
-module.exports = model('User', userSchema);
->>>>>>> development
